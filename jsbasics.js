69f07dfb88eaf70d639aaaed3c4f6bc4bede92c8
Avinash <er.avinashpanchal@gmail.com>
2024-07-30 13:18:06 +0530

    first commit

---
+++ jsbasics.js
@@ -0,0 +1,20 @@
+console.log("JavaScript created in 1995 by Brendan Eich while he was an engineer at Netscape");
+console.log("Variables in JavaScript store data values. They can be declared using var, let, or const.");
+console.log("The latest js version of JS is ES14 (ECMAScript 2023) in 2023");
+console.log("ECMAScript (ES) is the standardized scripting language specification upon which JavaScript is based. It standardizes the core features and functionalities of JavaScript.");
+console.log("ES6, also known as ECMAScript 2015, is the sixth edition of the ECMAScript standard. It introduced significant improvements and new features to JavaScript, including arrow functions, classes, template literals, and more.");
+
+/*
+JavaScript can be added to HTML file in two ways:
+1. Internal JS: 
+In this approach, you place JavaScript code directly within the <script> tags in the <head> or <body> section of your HTML page.
+<script>
+document.getElementById("demo").innerHTML = "My First JavaScript";
+</script>
+
+2. External JS:
+We can write JavaScript code in another files having an extension.js and then link this file inside the <head> tag of the HTML file in which we want to add this code.
+<script type="text/javascript" src="main.js">
+
+*/
+
