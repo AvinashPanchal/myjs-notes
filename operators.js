ff7fdc0a8070ed576a3383c38459865199b7ad11
Avinash <er.avinashpanchal@gmail.com>
2024-08-02 17:45:37 +0530

    Operators in JS

---
+++ operators.js
@@ -0,0 +1,87 @@
+//JavaScript Operators are symbols used to perform specific mathematical, comparison, assignment, and logical computations on operands.
+
+// JavaScript Arithmetic Operators:
+/*
+1. Addition (+) =>  Y = “Geeks” + “for” + “Geeks” gives , Y = “GeeksforGeeks” Y
+2. Subtraction (-) => Y = 5 – 3 gives, Y = 2
+3. Multiplication (*) => Y = 5 * 5 gives, Y = 25
+4. Division (/) => Y = 5 / 5 gives, Y = 1
+5. Modulus (%) => Modulus ‘%’ operator gives a remainder of an integer division. A % B means remainder (A/B) Y = 5 % 4 gives, Y = 1
+6. Exponentiation (**) => Y = 5 ** 3 gives, Y = 125
+7. Increment(++) => let A = 10 and Y = A + + then A = 11, Y=10
+8. Decrement (–-) => let A = 10 and Y = A – – then, A = 9 Y=10
+9. Unary (+) => +a means a is a positive number
+10. Negation (-) => -a means a is a negative number
+*/
+
+// JavaScript Assignment Operators:
+/*
+1. Assignment (=) => If A = 10 and Y = A then Y = 10
+2. Addition Assignment (+=) => Y += 1 gives Y = Y + 1
+3. Subtraction Assignment (-=) => Y -= 1 gives Y = Y – 1
+4. Multiplication Assignment (*=) => Y *= A is equivalent to Y = Y * A
+5. Division Assignment (/=) => Y /= A is equivalent to Y = Y / A
+6. Modules/Remainder Assignment (%=) => Y %= A is equivalent to Y = Y % A
+7. Exponentiation Assignment (**=) =>Y **= A is equivalent to Y=Y ** A
+8. Left Shift Assignment (<<=) => Y <<= A is equivalent to Y=Y << A
+9. Right Shift Assignment (>>=) => Y >>= A is equivalent to Y = Y >> A
+10. Bitwise AND Assignment (&=) => Y &= b is equivalent to Y = Y & A
+11. Bitwise OR Assignment (|=) => Y |= A is equivalent to Y= Y | b
+12. Bitwise XOR Assignment (^=) => Y ^= A is equivalent to Y= Y ^ A
+*/
+
+// JavaScript Comparison Operators
+/*
+1. Equality (==) => Y = 5 and X = 6 Y = = X is false.
+2. Strict equality (===) => Y = 5 and X = ‘5’ Y = = = X is false. (data and datatypes both are same)
+3. Inequality (!=) => let X = 10 then X ! = 11 is true.
+4. Strict inequality (!==) => let X = 10 then X ! == ’10’ is true.
+5. Greater than (>) => let X = 10 then X > 11 is false.
+6. Less than (<) => let X = 10 then X < 11 is true.
+7. Greater than or Equal to (>=) => let X = 10 then X > = 11 is false.
+8. Less than or Equal to (<= ) => let X = 10 then X < = 10 is true.
+*/
+
+// JavaScript Logical Operators
+/*
+1. Logical AND (&&) => Y = 5 and X = 6 Y && X is 6.
+2. Logical OR (||) => Y = 5 and X = 0 Y || X is 5.
+3. Logical NOT (!) => Y = 5 and X = 0 !(Y || X) is false.
+*/
+
+// JavaScript Ternary Operators: The ternary operator has three operands. It is the simplified operator of if/else.
+/*
+Ternary Operator (?:) => Y = ? A : B If the condition is true then Y = A otherwise Y = B
+*/
+
+//JavaScript Comma Operators
+/*
+Comma Operator (,) mainly evaluates its operands from left to right sequentially and returns the value of the rightmost operand. 
+*/
+
+// JavaScript Unary Operators: A unary operation is an operation with only one operand.
+/*
+1. Typeof => typeof variable
+2. Delete => delete object // or delete object.property  // or delete object[‘property’]
+*/
+
+// JavaScript Relational Operators: JavaScript Relational operators are used to compare its operands and determine the relationship between them.
+/*
+1. IN => propNameOrNumber in objectName
+2. instanceof => objectName instanceof objectType
+*/
+
+// JavaScript BigInt Operators : It returns a new BigInt object that represents the original value.
+/*
+BigInt => BigInt(value);
+*/
+
+// JavaScript String Operators: 
+/*
+concatenation operator (+) => str1 + str2
+*/
+
+//Operator precedence in JavaScript
+/*
+( * ) and ( / ) have higher precedence than ( + ) and ( - )
+*/
